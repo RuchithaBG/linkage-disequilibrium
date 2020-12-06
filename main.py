be90c63deda04429311101fe4e9784b7e8482594
RuchithaBG <ruchithabg7@gmail.com>
2020-12-06 19:54:53 +0530

    Added timeseries and made number of males and females equal in all generations.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ recom = 0.1
 n_mates = 1
 n_offsprings = 2 #  No. of offsprings per mating
 N = 4 #  Change population size later
+NoG = 2
 
 X = [['A','B'] for i in range(N)]
 Y = [['a','b'] for i in range(N)]
@@ -37,10 +38,14 @@ def nextgen(Z, G, recom, n_mates, n_offsprings):
     M_list = [Z[i] for i in range(len(Z)) if G[i]==0]
     F_list = [Z[i] for i in range(len(Z)) if G[i]!=0]
     Z = []
-    G = []
     for i in range(len(F_list)):
-        Z = Z +mate(F_list[i], M_list, recom, n_mates, n_offsprings)
-    G = [rn.choice([0,1]) for i in range(len(Z))]
-    return(Z, G)
+        Z += mate(F_list[i], M_list, recom, n_mates, n_offsprings)
+    return(Z)
 
-print(nextgen(Z, G, recom, n_mates, n_offsprings))
+def timeseries(NoG, Z, G, recom, n_mates, n_offsprings):
+    pop = [Z]
+    for i in range(NoG):
+        pop += [nextgen(Z, G, recom, n_mates, n_offsprings)]
+    return(pop)
+
+print(timeseries(NoG, Z, G, recom, n_mates, n_offsprings))
